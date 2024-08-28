3cf6e84386a069a053c873cc03b049ba586016e7
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-08-28 11:08:10 +0900

    [Solved] Problem_15988

---
+++ 24.08.28/정은지_15988.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4,1000001):
+    dp[i] = ((dp[i-3]+dp[i-2]+dp[i-1]) % 1000000009)
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
