0b6c79957d9060c894a92d743067179aa055691a
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-09-10 22:27:04 +0900

    [Solved] Problem_2156

---
+++ 24.09.10/정은지_2156.py
@@ -0,0 +1,17 @@
+n = int(input())
+array = [0] * 10000
+
+for i in range(n):
+  array[i] = int(input())
+
+dp = [0]*10000
+dp[0] = array[0]
+dp[1] = array[0] + array[1]
+dp[2] = max(array[2]+array[0], array[2]+array[1], d[1])
+
+for i in range(3,n):
+  dp[i] = max(array[i] + dp[i-2], 
+             array[i] + array[i-1] + dp[i-3], 
+             dp[i-1])
+
+print(max(dp))
