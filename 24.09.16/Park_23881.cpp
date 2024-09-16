680528049413b72be9866c378a0f80dbe62ff9bb
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2024-09-16 10:25:26 +0900

    [Init] 240916

---
+++ 24.09.16/Park_23881.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main (){ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+    int n;
+
+}
