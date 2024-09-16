da53dc328e3f33b07226fc2d422b39b848c012d5
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2024-09-16 12:57:57 +0900

    [Solved] Problem_23881

--- 24.09.16/Park_23881.cpp
+++ 24.09.16/Park_23881.cpp
@@ -5,6 +5,35 @@
 using namespace std;
 
 int main (){ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
-    int n;
+    int n, cnt;
+    cin >> n >> cnt;
+    vector<int> nums(n);
+    vector<pair<int,int>> res;
+    for(int k = 0; k < n; k++){
+        cin >> nums[k];
+    }
 
+    for(int k = n - 1; k > 0; k--){
+        int maxN = nums[k];
+        int idx = k;
+        bool isChange = false;
+        for(int j = k-1; j >= 0; j--){
+            if(nums[j] > maxN){
+                isChange = true;
+                maxN = nums[j];
+                idx = j;
+            }
+        }
+
+        if(isChange){
+            nums[idx] = nums[k];
+            nums[k] = maxN;
+            res.push_back({nums[idx], nums[k]});
+        }
+    }
+
+    if(res.size() <= cnt - 1)
+        cout << -1;
+    else
+        cout << res[cnt - 1].first << " " << res[cnt - 1].second;
 }
