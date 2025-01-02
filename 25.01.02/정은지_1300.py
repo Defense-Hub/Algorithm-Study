920338d789744274d0117ab6e28d19182c052799
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-01-02 17:09:29 +0900

    [Solved] Problem_1300

---
+++ 25.01.02/정은지_1300.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def Add(idx, n):
+    cnt = 0
+    for i in range(1, n+1):
+        cnt += min(idx//i, n)
+    return cnt
+
+def BinarySearch(n, k):
+    left = 1
+    right = k
+
+    while left <= right:
+        mid = (left + right) // 2
+        if countNum(mid, n) < k:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+
+n = int(input())
+k = int(input())
+
+answer = BinarySearch(n, k)
+print(answer)
